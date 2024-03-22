ce9761e6057790105c5a6f9cbeb52293448eed48
JeffDanielle <bayotjeff@gmail.com>
2024-03-22 20:43:43 +0800

    99. Adding a New Friend -19min

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,26 @@ import FormAddFriend from './components/FormAddFriend';
 import FormSplitBill from './components/FromSplitBill';
 import Button from './components/Button';
 import { useState } from 'react';
+import data from "./data/data"
 
 function App() {
   const [showAddFriend, setShowAddFriend] = useState(false);
+  const [friends, setFriends] = useState(data)
 
   const handleShowAddFriend = () => {
+    setShowAddFriend(prevState => !prevState)
+  }
+
+  const handleAddFriend = (friend) => {
+    setFriends(prevObj => [...prevObj, friend])
     setShowAddFriend(prevState => !prevState)
   }
 
   return (
     <div className='app'>
       <div className="sidebar">
-        <FriendList />
-        {showAddFriend && <FormAddFriend />}
+        <FriendList friendsProp={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
         <Button onClick={handleShowAddFriend}>{showAddFriend ? "Close" : "Add friend"}</Button>
       </div>
 
--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,10 +1,9 @@
 import { list } from 'postcss';
 import data from '../data/data'
 import Friend from './Friend';
-const FriendList = () => {
-    const friends = data;
+const FriendList = ({ friendsProp }) => {
     return (
-        <ul>{friends.map((friend) => <Friend key={friend.id} friend={friend} />)}</ul>
+        <ul>{friendsProp.map((friend) => <Friend key={friend.id} friend={friend} />)}</ul>
     );
 }
 
