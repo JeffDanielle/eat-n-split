ad7ce2b47b8f92ff9838a2ff845c5635f302228a
JeffDanielle <bayotjeff@gmail.com>
2024-03-23 13:19:28 +0800

    101. Creating Controlled Elements -10min

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,5 +1,6 @@
 import Friend from './Friend';
 const FriendList = ({ friendsProp, onSelection, selectedFriend }) => {
+    console.log(friendsProp)
     return (
         <ul>{friendsProp.map((friend) => <Friend key={friend.id} friend={friend} selectedFriend={selectedFriend} onSelection={onSelection} />)}</ul>
     );
